ae30619c7fcb2a5ec3568ae2e1fab38668c64f1e
Luiz Roberto <passageiro1829@gmail.com>
2022-07-16 02:45:44 -0300

    criando App flutter para monitorar despesas semanais contendo descrição, valor e hora atual da postagem

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: const Text("Grafico"),
+              elevation: 5,
+            ),
+          ),
+          TransactionUser(),
+        ],
+      ),
+    );
+  }
+}
